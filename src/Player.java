041ab3faabcc837a22fd2940b3908b4ac3093ceb
niiicky <nickylam234@gmail.com>
2023-05-31 17:46:42 -0400

    .

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,18 @@
 
 public class Player {
 
+	private String playerName;
+	private int HP;
+	private int playerX;
+	private int playerY;
+	// attacks
+	// items
+	// skin
+	
+	public Player(String name, int HP, int posX, int posY) {
+		playerName = name;
+		this.HP = HP;
+		playerX = posX;
+		playerY = posY;
+	}
 }
