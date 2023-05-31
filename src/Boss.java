4e6386ee9b91ac77171df3458062b3d6b7d76e99
Orion Chen <orion.chen@hotmail.com>
2023-05-31 18:17:38 -0400

    fix

---
+++ src/Boss.java
@@ -0,0 +1,23 @@
+
+public class Boss {
+	public int health;
+	public String name;
+	
+	
+	
+	public Boss(String name, int health) {
+		this.name = name;
+		this.health = health;
+	}
+	
+	
+	// getter
+	public String getName() {
+		return name;
+	}
+	public int getHealth() {
+		return health;
+	}
+	
+
+}
---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.util.*;
+
+public class Main extends JPanel implements KeyListener, MouseListener, Runnable{
+
+	
+	public Main() {
+		setPreferredSize(new Dimension(1000, 600));
+		setBackground(new Color(255, 255, 255));
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("wagwan");
+		JFrame frame = new JFrame("OMORI");
+		Main panel = new Main();
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
