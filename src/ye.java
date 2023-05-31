a03a339dc3c6629a682ab23227862dbffec04f89
niiicky <nickylam234@gmail.com>
2023-05-31 19:29:37 -0400

    testing2

--- src/ye.java
+++ src/ye.java
@@ -9,7 +9,7 @@ public class ye {
 
 	public static void main(String[] args) throws IOException {
 		
-		System.out.println("TEsting");
+		System.out.println("number");
 	}
 
 	static String next() throws IOException {
