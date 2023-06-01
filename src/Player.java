faa24ca9ed12ef1e4c5c73ae1941ecec5e29cbb7
niiicky <85355923+niiicky@users.noreply.github.com>
2023-06-01 08:50:29 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,54 @@
 
-public class Player {
+
+import javax.swing.*;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.util.*;
+
+public class Player extends JPanel implements Runnable{
+
+	public Main game;
+	
+	public Player(Main e) {
+		game = e;
+	}
+
+	int key = 0;
+	boolean legUp, legDown, legLeft, legRight;
+
+	@Override
+	public void run() {
+
+		// TODO Auto-generated method stub
+		if(key == 1) {
+			if(legUp) game.playerIndex = 9;
+			else game.playerIndex = 11;
+			legUp = !legUp;
+		}
+		else if(key == 2) {
+			if(legLeft) game.playerIndex = 3;
+			else game.playerIndex = 5;
+			legLeft = !legLeft;
+		}
+		else if(key == 3) {
+			if(legDown) game.playerIndex = 0;
+			else game.playerIndex = 2;
+			legDown = !legDown;
+		}
+		else {
+			if(legRight) game.playerIndex = 6;
+			else game.playerIndex = 8;
+			legRight = !legRight;
+		}
+		
+		game.repaint();
+	}
 
 }
